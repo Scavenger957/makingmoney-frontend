c8ac85d95379660915a4893f64e63d297dcf159b
Luigi <primek190@gmail.com>
2025-04-07 21:51:42 -0500

    Agrega App.js con interfaz funcional

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>MakingMoney</h1>
+      <p>¡Conectado y funcionando!</p>
+    </div>
+  );
+}
+
+export default App;
